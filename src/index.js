91519ac4fe88d374bd320f0497f76ba49a768e08
Artur199523 <artur.araqelyan1995@gmail.com>
2021-10-23 16:26:50 +0400

    test commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {Provider} from "react-redux";
+import RootReducer from './Redux/Reducers/RootReducers'
+import {applyMiddleware, createStore} from 'redux'
+import ReduxThunk from 'redux-thunk'
+const MyStore = createStore(RootReducer,applyMiddleware(ReduxThunk))
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={MyStore}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
